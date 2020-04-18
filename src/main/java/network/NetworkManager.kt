3bb70caf8ff07bc2681179a19263ab8017d4c35b
Mihael Berčič <mihael.register@gmail.com>
2020-04-18 18:11:20 +0200

    Added DHT basic functionality, joinRequest / onJoin and improved Message structure.

--- src/main/java/network/NetworkManager.kt
+++ src/main/java/network/NetworkManager.kt
@@ -2,20 +2,30 @@ package network
 
 import io.javalin.Javalin
 import io.javalin.http.Context
+import protocols.DHT
+import java.security.KeyPair
 
 /**
  * Created by Mihael Valentin Berčič
  * on 27/03/2020 at 12:58
  * using IntelliJ IDEA
  */
-class NetworkManager(port: Int, maxNodes: Int) {
+class NetworkManager(port: Int, maxNodes: Int, private val keyPair: KeyPair) {
 
-    private val nodeNetwork = NodeNetwork(maxNodes)
+    private val nodeNetwork = NodeNetwork(maxNodes, keyPair)
     private val application = Javalin.create { it.showJavalinBanner = false }.start(port)
 
+
+    // Protocols
+    private val dhtProtocol: DHT = DHT(nodeNetwork)
+
     init {
+
+        // TODO Send Join Request to main Node and wait for response on /joined
+
         "/ping" get { status(200) }
-        "/join" post { nodeNetwork.joinRequest(this) }
+        "/join" post { dhtProtocol.joinRequest(this) }
+        "/joined" post { dhtProtocol.onJoin(this) }
     }
 
     /**
--- src/main/java/network/NodeNetwork.kt
+++ src/main/java/network/NodeNetwork.kt
@@ -1,25 +1,27 @@
 package network
 
 import Main
+import abstraction.Message
 import abstraction.Node
-import io.javalin.http.Context
+import utils.Crypto
+import java.security.KeyPair
 
 /**
  * Created by Mihael Valentin Berčič
  * on 16/04/2020 at 13:45
  * using IntelliJ IDEA
  */
-class NodeNetwork(val maxNodes: Int) {
+class NodeNetwork(private val maxNodes: Int, private val keyPair: KeyPair) {
 
-    private val nodeMap: HashMap<String, Node> = hashMapOf()
+    // <PublicKey, Node>
+    val nodeMap: HashMap<String, Node> = hashMapOf()
+    var isInNetwork = false
+
+    val isFull get(): Boolean = nodeMap.size >= maxNodes
+
+    fun pickRandomNodes(amount: Int): List<Node> = nodeMap.values.shuffled().take(amount)
+    fun createMessage(text: String): Message = Message(keyPair.public.toString(), Crypto.sign(text, keyPair.private), text)
+    fun createMessage(any: Any): Message = Main.gson.toJson(any).let { json -> Message(keyPair.public.toString(), Crypto.sign(json, keyPair.private), json) }
 
 
-    fun joinRequest(context: Context) {
-        if (nodeMap.size < maxNodes) {
-            val node = Main.gson.fromJson(context.body(), Node::class.java)
-
-        } else pickRandomNodes(5).forEach {  }
-    }
-
-    fun pickRandomNodes(amount: Int) = nodeMap.values.shuffled().take(amount)
 }
