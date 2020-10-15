349f7156abba736caaae0333a557878957483d64
Mihael Berčič <mihael.register@gmail.com>
2020-10-15 15:10:17 +0200

    Code cleanup, code formatting and code refactoring has taken place. Also a lot of documentation code has been added.

--- src/main/java/manager/VDFManager.kt
+++ src/main/java/manager/VDFManager.kt
@@ -1,4 +1,4 @@
-package utils
+package manager
 
 import logging.Logger.debug
 import logging.Logger.info
--- src/main/java/network/attestation/AttestationData.kt
+++
@@ -1,9 +0,0 @@
-package network.attestation
-
-/**
- * Created by Mihael Valentin Berčič
- * on 21/09/2020 at 15:30
- * using IntelliJ IDEA
- */
-
-// TODO AttestationVote, AttestationData
