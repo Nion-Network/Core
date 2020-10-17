9a9b2b3de7eb172be349ef80577003a5d6ec07e5
Aleksandar Tošić <tosaleks@gmail.com>
2020-10-17 20:47:54 +0200

    influx tidyup

--- src/main/java/manager/DashboardManager.kt
+++ src/main/java/manager/DashboardManager.kt
@@ -2,11 +2,13 @@ package manager
 
 import data.Block
 import data.BlockVote
+import logging.Logger
 import org.apache.commons.codec.digest.DigestUtils
 import org.influxdb.InfluxDB
 import org.influxdb.InfluxDBFactory
 import org.influxdb.dto.Point
 import org.influxdb.dto.Query
+import java.util.concurrent.TimeUnit
 
 private lateinit var influxDB: InfluxDB
 class DashboardManager(private val applicationManager: ApplicationManager, val configuration: data.Configuration = applicationManager.configuration) {
@@ -15,19 +17,24 @@ class DashboardManager(private val applicationManager: ApplicationManager, val c
         influxDB = InfluxDBFactory.connect(configuration.influxUrl,configuration.influxUsername,configuration.influxPassword)
         influxDB.query(Query("CREATE DATABASE PROD"));
         influxDB.setDatabase("PROD")
+        //influxDB.setLogLevel(InfluxDB.LogLevel.FULL)
         if(influxDB.ping().isGood) println("InfluxDB connection successful")
     }
 
     fun newBlockProduced(blockData: Block){
         val point:Point = Point.measurementByPOJO(blockData.javaClass).addFieldsFromPOJO(blockData).build()
         influxDB.write(point)
+        influxDB.flush()
     }
 
     fun newVote(vote: BlockVote, publicKey: String){
-        val point = Point.measurement("vote").addField("blockHash", vote.blockHash)
+        Logger.debug("Sending attestation: ${DigestUtils.sha256Hex(vote.signature)} to Influx")
+        val point = Point.measurement("attestations")
+                .addField("blockHash", vote.blockHash)
                 .addField("signature", DigestUtils.sha256Hex(vote.signature))
-                .addField("validator", DigestUtils.sha256Hex(publicKey)).build()
+                .addField("committeeMember", publicKey).build()
         influxDB.write(point)
+        influxDB.flush()
     }
 }
 
