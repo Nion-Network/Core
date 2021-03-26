d158f330a9c24cdfef32657ea730fa381e91840a
Mihael Berčič <mihael.register@gmail.com>
2021-03-26 18:48:21 +0100

    Modified testnet to create nodes slower. Disabled logging for nodes. Modified timestamp print in Logger.

--- testnet.sh
+++ testnet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 docker build --tag node .
-for i in {1..9}; do (docker run -d -v /var/run/docker.sock:/var/run/docker.sock node)
+for i in {1..9}; do 
+(docker run -d -v /var/run/docker.sock:/var/run/docker.sock node)
+sleep 2
 done;
 
