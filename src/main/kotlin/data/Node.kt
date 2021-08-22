0ab11333599e5de63f5d793f3aa296e641e4a93d
Mihael Berčič <mihael.register@gmail.com>
2021-08-22 13:45:50 +0200

    Modified documentation for Node class.

--- src/main/kotlin/data/Node.kt
+++ src/main/kotlin/data/Node.kt
@@ -6,15 +6,8 @@ import kotlinx.serialization.Serializable
  * Created by Mihael Valentin Berčič
  * on 10/08/2021 at 01:37
  * using IntelliJ IDEA
- */
-
-/**
- * Stores information of some Node in the network.
  *
- * @property publicKey
- * @property ip
- * @property port
- * @property returnAddress String representing URL to access the Node.
+ * Stores information of some Node in the network.
  */
 @Serializable
 data class Node(val publicKey: String, val ip: String, val port: Int)
