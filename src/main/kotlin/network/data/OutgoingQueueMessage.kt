06dab2e22402f72ecb3c6f704802295532e88a21
Mihael Berčič <mihael@regnum.si>
2022-01-02 12:49:51 +0100

    Added documentation comments.

--- src/main/kotlin/network/data/OutgoingQueueMessage.kt
+++ src/main/kotlin/network/data/OutgoingQueueMessage.kt
@@ -8,6 +8,8 @@ import network.data.communication.TransmissionType
  * Created by mihael
  * on 10/12/2021 at 20:58
  * using IntelliJ IDEA
+ *
+ * This class stores data ready to be sent through UDP socket in [Server].
  */
 class OutgoingQueuedMessage(
     val endpoint: Endpoint,
