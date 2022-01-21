d7defbd4044428ed2f7c7906be75c2fd39801f68
Mihael Berčič <mihael@regnum.si>
2022-01-21 17:14:17 +0100

    Added argument parsing.

--- src/main/kotlin/network/SocketHolder.kt
+++ src/main/kotlin/network/SocketHolder.kt
@@ -11,7 +11,9 @@ import java.net.ServerSocket
  */
 open class SocketHolder(config: Configuration) {
 
-    protected val udpSocket: DatagramSocket = DatagramSocket(0)
+    protected val udpSocket: DatagramSocket = DatagramSocket(0).apply {
+        println("Bind to port: $port")
+    }
     protected val tcpSocket: ServerSocket = ServerSocket(0)
     protected val kademliaSocket: DatagramSocket = if (config.passedPort != -1) DatagramSocket(config.passedPort) else DatagramSocket(0)
 
