cc69b45e64f9f958929020d8b6ac4f0485b79f10
Mihael Berčič <mihael@regnum.si>
2022-01-24 17:05:55 +0100

    Switched to TCP.

--- src/main/kotlin/network/Server.kt
+++ src/main/kotlin/network/Server.kt
@@ -51,8 +51,8 @@ abstract class Server(val configuration: Configuration) : Kademlia(configuration
         Logger.info("We're the trusted node: $isTrustedNode | $localAddress:${localNode.udpPort}:${localNode.tcpPort}:${localNode.kademliaPort}.")
         if (started) throw IllegalStateException("Nion has already started.")
         startHistoryCleanup()
-        Thread(this::listenForUDP).start()
-        Thread(this::sendUDP).start()
+        Thread(this::listenForTCP).start()
+        Thread(this::sendTCP).start()
         Thread(this::processReceivedMessages).start()
         started = true
     }
