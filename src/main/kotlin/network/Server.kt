f5c43df513ec95380661c3dfdaf05ff43c601eeb
Mihael Berčič <mihael@regnum.si>
2022-01-24 09:56:34 +0100

    Switched to UDP.

--- src/main/kotlin/network/Server.kt
+++ src/main/kotlin/network/Server.kt
@@ -52,8 +52,8 @@ abstract class Server(val configuration: Configuration) : Kademlia(configuration
         Logger.info("We're the trusted node: $isTrustedNode | $localAddress:${localNode.udpPort}:${localNode.tcpPort}:${localNode.kademliaPort}.")
         if (started) throw IllegalStateException("Nion has already started.")
         startHistoryCleanup()
-        Thread(this::listenForTCP).start()
-        Thread(this::sendTCP).start()
+        Thread(this::listenForUDP).start()
+        Thread(this::sendUDP).start()
         Thread(this::processReceivedMessages).start()
         started = true
     }
