5ac0e53e68fbe2de90cf1c303760683764707119
Mihael Berčič <mihael@regnum.si>
2022-02-11 15:11:47 +0100

    Generated tree printing method that can be applied to any tree.

--- src/main/kotlin/utils/TreeUtils.kt
+++ src/main/kotlin/utils/TreeUtils.kt
@@ -38,4 +38,43 @@ object TreeUtils {
         val lastChildIndex = k * (index + 1)
         return firstChildIndex..lastChildIndex
     }
+
+    fun outputTree(k: Int, nodes: List<Any>): String {
+        val stringBuilder = StringBuilder()
+        val depth = computeDepth(k, nodes.lastIndex)
+        val distances = mutableMapOf<Int, Double>()
+        nodes.reversed().forEachIndexed { i, key ->
+            val index = nodes.size - 1 - i
+            val currentDepth = computeDepth(k, index)
+            val totalNodes = computeTotalNodesOnDepth(k, currentDepth)
+            val minimumIndex = computeMinimumIndexAtDepth(k, totalNodes, currentDepth)
+            val maximumIndex = computeMaximumIndexAtDepth(totalNodes)
+            val neighbourIndex = (index + 1).takeIf { it <= maximumIndex } ?: minimumIndex
+            if (currentDepth == depth) {
+                val center = (minimumIndex + maximumIndex) / 2.0
+                val distance = index - center
+                distances[index] = distance
+            } else {
+                val children = findChildren(k, index).mapNotNull { nodes.getOrNull(it) }
+                val median = children.sumOf { distances[nodes.indexOf(it)]!! } / k
+                val childrenConnections = children.joinToString("\n") { "\"$key\" -> \"$it\"" }
+                distances[index] = median
+                stringBuilder.append(childrenConnections + "\n")
+            }
+            val ourDistance = distances[index]!!
+            val neighbourChildren = findChildren(k, neighbourIndex).filter { it <= nodes.size - 1 }
+            val neighbourChildrenConnections = neighbourChildren.joinToString("\n") { "\"$key\" -> \"${nodes[it]}\" [color=\"red\"]" }
+
+            stringBuilder.apply {
+                val length = "$key".length
+                append(neighbourChildrenConnections + "\n")
+                nodes.getOrNull(neighbourIndex)?.apply {
+                    append("\"$key\" -> \"$this\" [color=\"green\"]\n")
+                }
+                append("\"$key\" [pos=\"${length * ourDistance},-${length * currentDepth}!\"]\n")
+            }
+        }
+        return stringBuilder.toString()
+        // Toolkit.getDefaultToolkit().systemClipboard.setContents(StringSelection(stringBuilder.toString()), null)
+    }
 }
