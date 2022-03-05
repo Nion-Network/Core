563bdbbca9a8a6f089dc76180d0d64bca30e0602
Mihael Berčič <mihael@regnum.si>
2022-03-06 00:27:34 +0100

    Added docker flag for daemon app running.

--- stress.sh
+++ stress.sh
@@ -3,7 +3,7 @@ count=200
 echo "Running stress test of $count apps..."
 
 for i in $(seq $count); do
-   docker run dormage/java-stress
+   docker run -d dormage/java-stress
    echo "Started $i-th app."
    sleep 30
 done
