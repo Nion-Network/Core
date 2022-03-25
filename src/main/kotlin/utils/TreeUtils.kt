46145730a1f3fdf63a9f15b7f88b4c6534795f6d
Mihael Berčič <mihael@regnum.si>
2022-03-25 20:18:34 +0100

    Tree output fixed for non n % k == 0 trees.

--- src/main/kotlin/utils/TreeUtils.kt
+++ src/main/kotlin/utils/TreeUtils.kt
@@ -1,5 +1,7 @@
 package utils
 
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
 import kotlin.math.ceil
 import kotlin.math.log10
 import kotlin.math.pow
@@ -39,46 +41,53 @@ object TreeUtils {
         return firstChildIndex..lastChildIndex
     }
 
-    fun outputTree(k: Int, nodes: List<Any>): String {
+    fun outputTree(k: Int, nodes: List<Any?>): String {
         val stringBuilder = StringBuilder()
         val depth = computeDepth(k, nodes.lastIndex)
         val distances = mutableMapOf<Int, Double>()
-        nodes.reversed().forEachIndexed { i, key ->
-            val index = nodes.size - 1 - i
-            val currentDepth = computeDepth(k, index)
-            val totalNodes = computeTotalNodesOnDepth(k, currentDepth)
-            val minimumIndex = computeMinimumIndexAtDepth(k, totalNodes, currentDepth)
-            val maximumIndex = computeMaximumIndexAtDepth(totalNodes)
-            val neighbourIndex = (index + 1).takeIf { it <= maximumIndex } ?: minimumIndex
+        val totalNodes = computeTotalNodesOnDepth(k, depth)
+        val neededNodes = (0 until totalNodes - nodes.size).map { null }.toList()
+        val filled = nodes.reversed().plus(neededNodes)
+
+        filled.forEachIndexed { i, key ->
+            val index = filled.lastIndex - i
+            val currentDepth = TreeUtils.computeDepth(k, index)
+            val nodesOnDepth = TreeUtils.computeTotalNodesOnDepth(k, currentDepth)
+            val minIndex = computeMinimumIndexAtDepth(k, nodesOnDepth, currentDepth)
+            val maxIndex = computeMaximumIndexAtDepth(nodesOnDepth)
+
             if (currentDepth == depth) {
-                val center = (minimumIndex + maximumIndex) / 2.0
+                val center = (minIndex + maxIndex) / 2.0
                 val distance = index - center
                 distances[index] = distance
             } else {
-                val children = findChildren(k, index).mapNotNull { nodes.getOrNull(it) }
-                val median = children.sumOf { distances[nodes.indexOf(it)]!! } / k
-                val childrenConnections = children.joinToString("\n") { "\"$key\" -> \"$it\"" }
+                val children = findChildren(k, index).filter { filled.lastIndex >= it }
+                val median = children.sumOf { distances[it]!! } / k
                 distances[index] = median
-                if (childrenConnections.isNotEmpty()) {
-                    stringBuilder.append(childrenConnections + "\n")
-                }
             }
-            val ourDistance = distances[index]!!
-            val neighbourChildren = findChildren(k, neighbourIndex).filter { it <= nodes.size - 1 }
-            val neighbourChildrenConnections = neighbourChildren.joinToString("\n") { "\"$key\" -> \"${nodes[it]}\" [color=\"red\"]" }
 
-            stringBuilder.apply {
-                val length = "$key".length
-                if (neighbourChildrenConnections.isNotEmpty()) {
-                    append(neighbourChildrenConnections + "\n")
-                }
-                nodes.getOrNull(neighbourIndex)?.apply {
-                    append("\"$key\" -> \"$this\" [color=\"green\"]\n")
-                }
-                append("\"$key\" [pos=\"${length * ourDistance},-${length * currentDepth}!\"]\n")
-            }
+            val distance = distances[index] ?: return@forEachIndexed
+            val node = nodes.getOrNull(index) ?: return@forEachIndexed
+
+            val neighbourIndex = (index + 1).takeIf { it <= maxIndex } ?: minIndex
+            val children = findChildren(k, index).mapNotNull { nodes.getOrNull(it) }
+            val neighbourChildren = findChildren(k, neighbourIndex).filter { it <= nodes.size - 1 }
+
+            val childrenConnections = children.joinToString("\n") { "\"$node\" -> \"$it\"" }
+            val neighbourChildrenConnections = neighbourChildren.joinToString("\n") { "\"$node\" -> \"${nodes[it]}\" [color=\"red\"]" }
+
+
+            val length = "$node".length
+            val neighbourNode = nodes.getOrNull(neighbourIndex)
+
+            if (neighbourNode != null) stringBuilder.append("\"$node\" -> \"$neighbourNode\" [color=\"green\"]\n")
+            if (childrenConnections.isNotEmpty()) stringBuilder.append(childrenConnections + "\n")
+            if (neighbourChildrenConnections.isNotEmpty()) stringBuilder.append(neighbourChildrenConnections + "\n")
+
+            stringBuilder.append("\"$node\" [pos=\"${length * distance},-${length * currentDepth}!\"]\n")
+            // stringBuilder.append("\"$node\" [pos=\"${distance},-$currentDepth!\"]\n")
         }
+        Toolkit.getDefaultToolkit().systemClipboard.setContents(StringSelection(stringBuilder.toString()), null)
         return stringBuilder.toString()
-        // Toolkit.getDefaultToolkit().systemClipboard.setContents(StringSelection(stringBuilder.toString()), null)
     }
 }
--- src/test/kotlin/MarryTreeTest.kt
+++ src/test/kotlin/MarryTreeTest.kt
@@ -1,8 +1,8 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import utils.TreeUtils
-import java.awt.Toolkit
-import java.awt.datatransfer.StringSelection
+import utils.asHex
+import utils.sha256
 
 /**
  * Created by mihael
@@ -55,42 +55,9 @@ class MarryTreeTest {
     @Test
     fun outputTreeConnections() {
         val k = 2
-        val depth = 2
+        val depth = 3
         val maximumNodes = TreeUtils.computeTotalNodesOnDepth(k, depth)
-        val tree = (0 until maximumNodes).toList()
-        val stringBuilder = StringBuilder()
-        val distances = mutableMapOf<Int, Double>()
-        /*̣̣tree.reversed().forEach { index ->
-            val currentDepth = TreeUtils.computeDepth(k, index)
-            val totalNodes = TreeUtils.computeTotalNodesOnDepth(k, currentDepth)
-            val minimumIndex = TreeUtils.computeMinimumIndexAtDepth(k, totalNodes, currentDepth)
-            val maximumIndex = TreeUtils.computeMaximumIndexAtDepth(totalNodes)
-            val neighbourIndex = (index + 1).takeIf { it <= maximumIndex } ?: minimumIndex
-            if (currentDepth == depth) {
-                val center = (minimumIndex + maximumIndex) / 2.0
-                val distance = index - center
-                distances[index] = distance
-            } else {
-                val children = TreeUtils.findChildren(k, index)
-                val median = children.sumOf { distances[it]!! } / k
-                val childrenConnections = children.joinToString("\n") { "$index -> $it" }
-                distances[index] = median
-                stringBuilder.append(childrenConnections + "\n")
-            }
-            val ourDistance = distances[index]!!
-            val neighbourChildren = TreeUtils.findChildren(k, neighbourIndex).filter { it <= maximumNodes - 1 }
-            val neighbourChildrenConnections = neighbourChildren.joinToString("\n") { "$index -> $it[color=\"red\"]" }
-
-            stringBuilder.apply {
-                append(neighbourChildrenConnections + "\n")
-                append("$index -> $neighbourIndex [color=\"green\"]\n")
-                append("$index [pos=\"$ourDistance,-$currentDepth!\"]\n")
-            }
-        }
-         */
-        // print(stringBuilder)
-
+        val tree = (0 until maximumNodes).toList().map { sha256("$it").asHex.substring(50..60) }
         print(TreeUtils.outputTree(k, tree))
-        // Toolkit.getDefaultToolkit().systemClipboard.setContents(StringSelection(TreeUtils.outputTree(k, tree)), null)
     }
 }
