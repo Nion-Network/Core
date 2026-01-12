43de0243f9aa88ce1b7164c90132ab94e43fc5ac
Mihael Berčič <mihael@regnum.si>
2026-01-12 17:09:52 +0100

    Fixed ws.

--- src/main/kotlin/network/rpc/RPCManager.kt
+++ src/main/kotlin/network/rpc/RPCManager.kt
@@ -77,7 +77,6 @@ open class RPCManager(configuration: Configuration) : Kademlia(configuration) {
         val serialisedData = Json.encodeToString(message)
         val clientList = subscribedClients[topic] ?: return
         clientList.forEach {
-            println("Websocket data: $serialisedData")
             try {
                 it.send(serialisedData)
             } catch (e: Exception) {
