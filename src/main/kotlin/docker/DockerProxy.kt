2a390e803d0a0e8b2a7253e76c8c998dd28de89d
Mihael Berčič <mihael@regnum.si>
2026-01-15 12:45:36 +0100

    Looking for issues with buffering.

--- src/main/kotlin/docker/DockerProxy.kt
+++ src/main/kotlin/docker/DockerProxy.kt
@@ -93,7 +93,9 @@ abstract class DockerProxy(configuration: Configuration) : MigrationStrategy(con
     private fun listenForDockerStatistics() {
         println("Started a docker stats process")
         val numberOfElements = (configuration.slotDuration / 1000).toInt()
-        val json = Json { ignoreUnknownKeys = true }
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
         while (true) {
             // TODO: I believe this could be optimised
             val process = ProcessBuilder()
@@ -103,6 +105,7 @@ abstract class DockerProxy(configuration: Configuration) : MigrationStrategy(con
             val reader = process.inputStream.bufferedReader()
 
             reader.readLines().forEach { line ->
+                println(line)
                 val stats = json.decodeFromString<DockerStatsModel>(line)
                 val pids = stats.pids.toIntOrNull() ?: 0
                 val cpuPercentage = stats.cpuPercentage.trim('%').toDoubleOrNull() ?: 0.0
